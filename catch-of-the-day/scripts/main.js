be43a9a15804ec03f23c5e0bb87b97218dc1f467
Chip Cullen <ccullen@pbs.org>
2016-06-09 10:53:24 -0400

    through video 12, fishes render correctly

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -62,9 +62,15 @@ var App = React.createClass({
 
 var Fish = React.createClass({
   render: function(){
+    var details = this.props.details;
     return (
       <li className="menu-fish">
-        <img src="{this.props.details.img}" />
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
       </li>
       )
   }
