95346671dbb158a7eb8085c22e00c1a462d5b941
Chip Cullen <ccullen@pbs.org>
2016-06-09 12:11:47 -0400

    through video 15 - got it synced up with firebase

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -9,6 +9,11 @@ var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
+
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catch-of-the-day-753b1.firebaseio.com');
+
 /*
   App
 */
@@ -19,6 +24,12 @@ var App = React.createClass({
       fishes: {},
       order: {}
     }
+  },
+  componentDidMount : function() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
   },
   addToOrder : function(key) {
     // updates the order to its current value plus one, or start with one
