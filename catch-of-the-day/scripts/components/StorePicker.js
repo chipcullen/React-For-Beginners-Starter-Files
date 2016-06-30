3bfb56a7716bf4083f18ce6a6244acbab442c94e
Chip Cullen <ccullen@pbs.org>
2016-06-30 15:49:08 -0400

    through video 25 - converted storepicker to class, using reactMixin and autobind decorator

--- catch-of-the-day/scripts/components/StorePicker.js
+++ catch-of-the-day/scripts/components/StorePicker.js
@@ -6,19 +6,22 @@
 import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins : [
-    History
-  ],
-  goToStore : function(event){
+
+@autobind
+class StorePicker extends React.Component {
+
+  goToStore(event) {
     event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
     // transition from store picker to app
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render: function(){
+  }
+
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
@@ -28,6 +31,8 @@ var StorePicker = React.createClass({
     )
   }
 
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
