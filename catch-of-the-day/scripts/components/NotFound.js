91583c0479d6c8797433707739e72a1bd0312219
Chip Cullen <ccullen@pbs.org>
2016-06-30 15:34:13 -0400

    through video 23 - converted not found to an ES6 class

--- catch-of-the-day/scripts/components/NotFound.js
+++ catch-of-the-day/scripts/components/NotFound.js
@@ -2,10 +2,12 @@
 
 import React from 'react';
 
-var NotFound = React.createClass({
-  render: function(){
+class NotFound extends React.Component {
+
+  render() {
     return <h1>Not Found!</h1>
   }
-});
+
+}
 
 export default NotFound;
