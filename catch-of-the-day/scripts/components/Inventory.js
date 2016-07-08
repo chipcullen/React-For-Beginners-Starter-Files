4f3db415a41d3485e60d8d82bffb9c4d88444378
Chip Cullen <ccullen@pbs.org>
2016-07-08 09:21:24 -0400

    midway through video 29 - firebase is being a pain

--- catch-of-the-day/scripts/components/Inventory.js
+++ catch-of-the-day/scripts/components/Inventory.js
@@ -5,9 +5,37 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 import autobind from 'autobind-decorator';
+import Firebase from 'firebase';
+const ref = new Firebase('https://catch-of-the-day-753b1.firebaseio.com');
 
 @autobind
 class Inventory extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      uid : ''
+    }
+  }
+
+  authenticate(provider) {
+    console.log('trying to auth with ' + provider );
+    ref.authWithOAuthPopup(provider, function(err, authData) {
+      console.log(authData);
+    });
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={this.authenticate.bind(this, 'github')}>Log In with Github</button>
+        <button className="twitter" onClick={this.authenticate.bind(this, 'twitter')}>Log In with Twitter</button>
+      </nav>
+    )
+  }
 
   renderInventory(key) {
     var linkState = this.props.linkState;
@@ -28,10 +56,32 @@ class Inventory extends React.Component {
   }
 
   render() {
+
+    let logoutButton = <button>Log Out!</button>
+
+    // first check if they are logged in
+    if(!this.state.uid) {
+      return (
+        <div>
+          { this.renderLogin() }
+        </div>
+      )
+    }
+
+    // then check if they aren't the owner
+    if(this.state.uid !== this.state.owner){
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of this store</p>
+          {logoutButton}
+        </div>
+      )
+    }
+
     return (
       <div>
         <h2>Inventory</h2>
-
+        {logoutButton}
         {Object.keys(this.props.fishes).map(this.renderInventory)}
 
         <AddFishForm { ...this.props } />
@@ -39,7 +89,9 @@ class Inventory extends React.Component {
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
+
   }
+
 };
 
 Inventory.propTypes = {
